75d8480190762e9cdd42f0e41368c88041d67215
Anastasia <anastasiaerofeeva121@gmail.com>
2025-08-01 15:59:41 -0700

    ignore file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -100,7 +100,7 @@ export default defineConfig([
       "react-hooks/exhaustive-deps": "off",
       "jsx-a11y/click-events-have-key-events": "warn",
       "jsx-a11y/interactive-supports-focus": "warn",
-      "prettier/prettier": "warn",
+      "prettier/prettier": "off",
       "no-unused-vars": "off",
       "unused-imports/no-unused-vars": "off",
       "unused-imports/no-unused-imports": "warn",
@@ -136,11 +136,11 @@ export default defineConfig([
             },
           ],
 
-          "newlines-between": "always",
+          "newlines-between": "ignore",
         },
       ],
 
-      "react/self-closing-comp": "warn",
+      "react/self-closing-comp": "off",
 
       "react/jsx-sort-props": [
         "warn",
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-
+import { Provider as ReduxProvider } from "react-redux";
+import { store } from "./store/store.ts";
 import App from "./App.tsx";
 import { Provider } from "./provider.tsx";
 import "@/styles/globals.css";
@@ -9,9 +10,11 @@ import "@/styles/globals.css";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider>
-        <App />
-      </Provider>
+      <ReduxProvider store={store}>
+        <Provider>
+          <App />
+        </Provider>
+      </ReduxProvider>
     </BrowserRouter>
   </React.StrictMode>,
 );
