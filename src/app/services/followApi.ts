4ab8acd13ea7aaa3d6a02ee1bacf0cad3ae22898
Anastasia <anastasiaerofeeva121@gmail.com>
2025-08-03 14:08:42 -0700

    follow api

---
+++ src/app/services/followApi.ts
@@ -0,0 +1,22 @@
+import { api } from "./api";
+
+export const followApi = api.injectEndpoints({
+  endpoints: (build) => ({
+    followUser: build.mutation<void, {followingId:string}>({
+      query: (id) => ({
+        url: '/follows',
+        method:'POST',
+        body:id
+      })
+    }),
+    unfollowUser: build.mutation<void, string>({
+      query: (id) => ({
+        url:`/follows/${id}`,
+        method:'DELETE'
+      })
+    })
+  })
+})
+
+export const {useFollowUserMutation, useUnfollowUserMutation} = followApi;
+export const {endpoints: {followUser, unfollowUser}} = followApi;
