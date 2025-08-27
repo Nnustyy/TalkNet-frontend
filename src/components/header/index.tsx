bcf61285821eecb4a2f2413d9ffdf5e41445aabc
Anastasia <anastasiaerofeeva121@gmail.com>
2025-08-27 13:45:31 -0700

    logout button

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,12 +1,27 @@
 import { Navbar, NavbarBrand, NavbarContent, NavbarItem } from '@heroui/navbar';
 import React, { useContext } from 'react';
 import {FaRegMoon, FaRegSun,} from 'react-icons/fa'
+import { CiLogout } from "react-icons/ci";
+import { useNavigate } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from '@heroui/button';
 import { ThemeContext } from '../themeProvider';
+import { logout, selectIsAuthenticated } from '@/features/user/userSlice';
 
 
 const Header = () => {
 
   const {theme, toggleTheme} = useContext(ThemeContext);
+  const isAuthenticated = useSelector(selectIsAuthenticated)
+  const dispatch = useDispatch();
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    dispatch(logout())
+    localStorage.removeItem('token')
+    navigate('/auth')
+  }
+
 
   return (
     <div>
@@ -19,7 +34,16 @@ const Header = () => {
         {theme === 'light' ? <FaRegMoon/> : <FaRegSun/> }
           </NavbarItem>
           <NavbarItem>
-            
+            {isAuthenticated && (
+              <Button
+              color='default'
+              variant='flat'
+              className='gap-2'
+              onPress={handleLogout}
+              >
+                <CiLogout/> <span>Выйти</span>
+              </Button>
+            )}
           </NavbarItem>
           </NavbarContent>
       </Navbar>
