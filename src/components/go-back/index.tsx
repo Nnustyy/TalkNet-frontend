33493560a49a5a0368d834eb785ba9b4b2e5cf31
Anastasia <anastasiaerofeeva121@gmail.com>
2025-09-01 22:10:37 +0300

    arrow go-back + comments render

---
+++ src/components/go-back/index.tsx
@@ -0,0 +1,25 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { RiArrowGoBackLine } from "react-icons/ri";
+
+const GoBack = () => {
+
+  const navigate = useNavigate();
+
+  const handleGoBack  = () => {
+    navigate(-1);
+  }
+
+  return (
+    <div 
+      className='flex text-default-500 cursor-pointer items-center gap-2 mb-10 '
+      onClick={handleGoBack}
+      >
+      <RiArrowGoBackLine />
+      Назад
+    </div>
+  );
+};
+
+export default GoBack;
--- src/pages/current-post/index.tsx
+++ src/pages/current-post/index.tsx
@@ -1,14 +1,68 @@
+import { useParams } from 'react-router-dom';
+import { useGetPostByIdQuery } from '@/app/services/postsApi';
+import Card from '@/components/card';
 import CreateComment from '@/components/create-comment';
 import React from 'react';
+import GoBack from '@/components/go-back';
 
 const CurrentPost = () => {
+
+  const params = useParams<{id:string}>();
+  const {data} = useGetPostByIdQuery(params?.id ?? '');
+
+  if(!data) {
+    return <h2>Такого поста не существует</h2>
+  }
+
+  const  {
+    author,
+    authorId,
+    comments,
+    content,
+    createdAt,
+    id,
+    likedByUser,
+    likes,
+  } = data
+
   return (
     <>
-    <CreateComment/>
-    <div className='mb-10 w-full'>
-      
+    <GoBack/>~
+    <div className='mb-5 w-full'>
+    <Card
+        key={id}
+        authorId={authorId}
+        cardFor='current-post'
+        commentsCount={comments.length}
+        content={content}
+        likedByUser={likedByUser}
+        likesCount={likes.length}
+        createdAt={createdAt}
+        avatarUrl={author.avatarUrl?? ''}
+        name={author.name ?? ''}
+        id={id}
+    />
     </div>
-
+    <CreateComment/>
+      <div className='mt-15'>
+        {comments && (
+          comments.length > 0 
+          ? comments.map(({id, content,createdAt,user,userId}) => 
+            <Card
+            key={id}
+            content={content}
+            createdAt={createdAt}
+            avatarUrl={user.avatarUrl ?? ''}
+            cardFor='comment'
+            name={user.name ?? ''}
+            id={id}
+            authorId={userId}
+  
+            />
+          )
+          : <h5 className="text-default-500 text-center">Оставьте первый комментарий!</h5>
+        )}
+      </div>
     </>
   );
 };
