3ccca9b91e9f0383ffad49120a9be8fae82d07cc
Anastasia <anastasiaerofeeva121@gmail.com>
2026-01-11 13:59:47 +0300

    constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
-// export const BASE_URL = process.env.NODE_ENV === 'production' ? 'http://localhost:3000' : 'http://localhost:3000'
 export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
