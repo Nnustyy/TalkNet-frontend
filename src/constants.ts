84b8059818633f13dc0915276605cbf757cebc48
Anastasia <anastasiaerofeeva121@gmail.com>
2026-01-11 15:36:59 +0300

    constants config

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
-// export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
-export const BASE_URL = 'https://talknet-backend-sge2.onrender.com';
+export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:3000';
